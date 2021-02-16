1b8442dd0360e01c37c3672ac38c49cb3f93c4e3
nguyen <>
2021-02-16 19:55:27 +0900

    first commit

---
+++ public/main.js
@@ -0,0 +1,59 @@
+
+const form = document.querySelector("form");
+function elementFactory(type,properties){
+   let element = document.createElement(type);
+   for(let prop in properties){
+      element.setAttribute(prop,properties[prop])
+   }
+   return element;
+}
+
+class ShoppingItem {
+   constructor(){
+      this.item = `<h2>Item</h2> <input name="item">`,
+      this.quantity = `<h2>Quantity</h2> <input name = "quantity">`
+   };
+}
+var count = 1;
+function createItem(){
+   count++;
+   const shoppingItem = new ShoppingItem();
+   const container = elementFactory("div",{class:"container", id: count});
+   container.innerHTML = shoppingItem.item + shoppingItem.quantity;
+   const remove = elementFactory("h3",{class:"remove", number: count});
+   remove.innerHTML = "Remove";
+   container.appendChild(remove);
+   form.appendChild(container);
+   
+   remove.addEventListener("click", function(event){
+      let x = event.currentTarget.getAttribute("number");
+      let response = window.confirm("Are you sure you want to remove this item?");
+      if(response){
+         document.getElementById(x).remove();
+      }
+   });
+}
+
+const add = document.querySelector(".add");
+add.addEventListener("click",function(){
+   createItem();
+})
+
+const collections = document.querySelectorAll(".collections");
+function makeRequest(modelName){
+   const xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        
+        
+      }
+   };
+   xhttp.open("GET","docs.js",true);
+   xhttp.send();
+}
+collections.forEach(collection => {
+   collection.addEventListener("click",function(){
+      const clickedModelName = collection.innerHTML;
+      makeRequest(clickedModelName)
+   })
+})
---
+++ server.js
@@ -0,0 +1,86 @@
+// jshint esversion:6
+/* const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose"); */
+import express from "express";
+import mongoose from "mongoose";
+export{getDocs}
+
+const app = express();
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+// bodyParser is included in express version 4 and above so there is no need to install it separately
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/shopDB",{useNewUrlParser:true,useUnifiedTopology: true});
+
+//define functions, variables, db schema etc
+const itemSchema = new mongoose.Schema({
+   item: String,
+   quantity: Number
+})
+
+const List1 = mongoose.model("List1",itemSchema);
+
+function getCollectionNames(){
+   return mongoose.connection.db.listCollections().toArray() 
+   .then(collectionArr => collectionArr.map(collection => collection.name))
+   .catch(err => console.log(err))
+}
+
+function getDocs(modelName){
+   return modelName.find()
+   .then(docs => docs)
+   .catch(err => console.log(err))
+}
+
+// functions in action
+app.get("/",function(req,res){
+
+let dbcollections = getCollectionNames();
+let docs = getDocs(List1);
+Promise.all([docs,dbcollections])
+.then(values => res.render("index",{list: values[0],collections: values[1]}))
+.catch(err => console.log(err))
+
+})
+
+
+app.post("/", function(req,res){
+   const itemName = req.body.item;
+   const quantityValue = req.body.quantity;
+   
+   if(itemName instanceof Array){
+
+      for(let i = 0; i < itemName.length; i++){
+         const query = {item: itemName[i].toLowerCase()};
+         const update = {item: itemName[i].toLowerCase(),quantity: quantityValue[i]};
+         List1.updateOne(query,update,{upsert:true},function(err){
+            if(err){
+               console.log(err)
+            }else{
+               console.log("successfully update many")
+            }
+         })
+      }
+   }else{
+      List1.updateOne({item: itemName.toLowerCase()},{quantity: quantityValue},{upsert:true},function(err){
+         if(err){
+            console.log(err)
+         }else{
+            console.log("successfully update one")
+         }
+      })
+   }
+  
+   res.redirect("/");
+ 
+})
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+   console.log("Server started on port 3000")
+})
