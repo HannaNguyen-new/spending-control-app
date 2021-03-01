27e50181977909edbdef94ceba9f9ced265e9bd0
HannaNguyen-new <62462223+HannaNguyen-new@users.noreply.github.com>
2021-03-01 19:44:48 +0900

    update

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 //the below url is to connect to mongodb atlas
 //const url = "mongodb+srv://NGUYEN_boss:q3bzOXv7VLtPAG3i@cluster0.jpxwg.mongodb.net/shopDB?retryWrites=true&w=majority"
 //Since we have set Mongodb atlas cluster to config var --> it is exposed to the application's code as environment variable 
-const url = process.env.MONGODB_URI;
+const  url = process.env.MONGODB_URI;
+
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true})
 .then(success => console.log("Connected to database"))
 .catch(err => console.log(err))
